e9e53c79e97640e3e2fea14463b0ec53d4f9498a
sapcraft-org <76895953+sapcraft-org@users.noreply.github.com>
2021-07-30 08:36:25 +0530

    Created and Bug fixed vol-1.a

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:.',
+        error: ':warning:',
+        queue: ':.bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+      token: 'ODcwNDgwNzUzMzc5NjU1NzMx.YQNYYA.EYJqLmV_cOSL6Dt1SbQkv3tS__c',
+        prefix: 's!',
+        activity: 'music with s!',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
---
+++ index.js
@@ -0,0 +1,58 @@
+var express = require("express");
+var http = require("http");
+var app = express();
+
+// Ping The Apps.
+app.use(express.static("public"));
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+    response.sendStatus(200);
+});
+
+// Request Listeners.
+var listener = app.listen(process.env.PORT, function () {
+    console.log("Coming Mister Sapphire " + listener.address().port);
+});
+setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 270000);
+
+const fs = require('fs');
+const discord = require('discord.js');
+
+const client = new discord.Client({ disableMentions: 'everyone' });
+
+const { Player } = require('discord-player');
+
+client.player = new Player(client);
+client.config = require('./config/bot');
+client.emotes = client.config.emojis;
+client.filters = client.config.filters;
+client.commands = new discord.Collection();
+
+fs.readdirSync('./commands').forEach(dirs => {
+    const commands = fs.readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
+
+    for (const file of commands) {
+        const command = require(`./commands/${dirs}/${file}`);
+        console.log(`Loading command ${file}`);
+        client.commands.set(command.name.toLowerCase(), command);
+    };
+});
+
+const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const player = fs.readdirSync('./player').filter(file => file.endsWith('.js'));
+
+for (const file of events) {
+    console.log(`Loading discord.js event ${file}`);
+    const event = require(`./events/${file}`);
+    client.on(file.split(".")[0], event.bind(null, client));
+};
+
+for (const file of player) {
+    console.log(`Loading discord-player event ${file}`);
+    const event = require(`./player/${file}`);
+    client.player.on(file.split(".")[0], event.bind(null, client));
+};
+
+client.login(client.config.discord.token);
